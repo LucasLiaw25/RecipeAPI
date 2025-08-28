b48c3030b37dfc021619d4ee2d6f1fdcdd50400f
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 19:15:16 -0300

    feat: Cria o dto de Recipe

---
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/dto/RecipeDTO.java
@@ -0,0 +1,13 @@
+package com.liaw.dev.Recipe.infrastructure.dto;
+
+import com.liaw.dev.Recipe.core.enums.Category;
+import jakarta.validation.constraints.NotBlank;
+
+public record RecipeDTO(
+        Long id,
+        @NotBlank String title,
+        @NotBlank String description,
+        @NotBlank String ingredient,
+        @NotBlank Category category
+) {
+}
