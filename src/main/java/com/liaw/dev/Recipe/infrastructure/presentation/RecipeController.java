e74345aa126a84610b06fbc888ab3d8351826301
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 19:18:22 -0300

    feat: Cria o controller da aplicação

---
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/presentation/RecipeController.java
@@ -0,0 +1,9 @@
+package com.liaw.dev.Recipe.infrastructure.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/recipe/")
+public class RecipeController {
+}
