2f29fae6894a91fea029a4c77a7b62d60708d15d
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 19:13:24 -0300

    feat: Implementa a interface de criação de receita

---
+++ src/main/java/com/liaw/dev/Recipe/core/usecase/CreateRecipeUseCaseImpl.java
@@ -0,0 +1,18 @@
+package com.liaw.dev.Recipe.core.usecase;
+
+import com.liaw.dev.Recipe.core.entities.Recipe;
+import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
+
+public class CreateRecipeUseCaseImpl implements CreateRecipeUseCase{
+
+    private final RecipeGateway recipeGateway;
+
+    public CreateRecipeUseCaseImpl(RecipeGateway recipeGateway) {
+        this.recipeGateway = recipeGateway;
+    }
+
+    @Override
+    public Recipe execute(Recipe recipe) {
+        return recipeGateway.createRecipe(recipe);
+    }
+}
