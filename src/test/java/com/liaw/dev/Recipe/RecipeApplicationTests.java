5528eff7a0c6c49b6cce6b0b1787964d16117213
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 18:49:49 -0300

    My first commit

---
+++ src/test/java/com/liaw/dev/Recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaw.dev.Recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
