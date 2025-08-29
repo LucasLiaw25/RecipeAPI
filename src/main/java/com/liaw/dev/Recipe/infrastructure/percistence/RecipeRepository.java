b084a2e509c497c24e78e16a7437caa1593d03d9
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-29 12:01:29 -0300

    feat: Declara metodos de procura de receitas

--- src/main/java/com/liaw/dev/Recipe/infrastructure/percistence/RecipeRepository.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/percistence/RecipeRepository.java
@@ -1,10 +1,19 @@
 package com.liaw.dev.Recipe.infrastructure.percistence;
 
+import com.liaw.dev.Recipe.core.entities.Recipe;
+import com.liaw.dev.Recipe.core.enums.Category;
 import com.liaw.dev.Recipe.infrastructure.percistence.RecipeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
+public interface RecipeRepository extends JpaRepository<RecipeEntity, Long>, QueryByExampleExecutor<RecipeEntity> {
     Optional<RecipeEntity> findByTitleAndDescription(String title, String description);
+    Optional<RecipeEntity> findByTitleAndDescriptionAndIngredient(
+            String title, String description, String ingredient
+    );
+    List<RecipeEntity> findByCategory(Category category);
+
 }
