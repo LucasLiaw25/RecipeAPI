dedd48b2483436b7b4698e6b2c8d89d2007af938
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 21:28:02 -0300

    feat: Cria o contexto para UseCaseImpl de listar, atualizar e deletar receitas

--- src/main/java/com/liaw/dev/Recipe/infrastructure/bean/RecipeConfiguration.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/bean/RecipeConfiguration.java
@@ -1,8 +1,10 @@
 package com.liaw.dev.Recipe.infrastructure.bean;
 
 import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
-import com.liaw.dev.Recipe.core.usecase.CreateRecipeUseCase;
-import com.liaw.dev.Recipe.core.usecase.CreateRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.usecase.classes.CreateRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.usecase.classes.DeleteRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.usecase.classes.ListRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.usecase.classes.UpdateRecipeUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,23 @@ import org.springframework.context.annotation.Configuration;
 public class RecipeConfiguration {
 
     @Bean
-    public CreateRecipeUseCase createRecipeUseCase(RecipeGateway gateway){
+    public CreateRecipeUseCaseImpl createRecipeUseCase(RecipeGateway gateway){
         return new CreateRecipeUseCaseImpl(gateway);
+    }
+
+    @Bean
+    public ListRecipeUseCaseImpl listRecipeUseCase(RecipeGateway gateway){
+        return new ListRecipeUseCaseImpl(gateway);
+    }
+
+    @Bean
+    public UpdateRecipeUseCaseImpl updateRecipeUseCase(RecipeGateway gateway){
+        return new UpdateRecipeUseCaseImpl(gateway);
+    }
+
+    @Bean
+    public DeleteRecipeUseCaseImpl deleteRecipeUseCase(RecipeGateway gateway){
+        return new DeleteRecipeUseCaseImpl(gateway);
     }
 
 }
