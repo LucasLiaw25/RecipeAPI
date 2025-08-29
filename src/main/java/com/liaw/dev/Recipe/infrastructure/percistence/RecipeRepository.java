9851faf90e19314d002eb56a657bc39c41cf265a
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 21:29:28 -0300

    feat: Declara metodo de findByTitleAndDescription

--- src/main/java/com/liaw/dev/Recipe/infrastructure/percistence/RecipeRepository.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/percistence/RecipeRepository.java
@@ -1,6 +1,9 @@
-package com.liaw.dev.Recipe.infrastructure.percistence;
+gpackage com.liaw.dev.Recipe.infrastructure.percistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
+    Optional<RecipeEntity> findByTitleAndDescription(String title, String description);
 }
