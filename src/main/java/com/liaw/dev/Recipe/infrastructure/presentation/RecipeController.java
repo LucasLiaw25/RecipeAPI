79ec865c58f4ba7774bccc5b3e7467cbac588ab4
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-29 12:02:08 -0300

    feat: Cria endpoints para pesquisa de receita

--- src/main/java/com/liaw/dev/Recipe/infrastructure/presentation/RecipeController.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/presentation/RecipeController.java
@@ -1,10 +1,8 @@
 package com.liaw.dev.Recipe.infrastructure.presentation;
 
 import com.liaw.dev.Recipe.core.entities.Recipe;
-import com.liaw.dev.Recipe.core.usecase.classes.CreateRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.DeleteRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.ListRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.UpdateRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.enums.Category;
+import com.liaw.dev.Recipe.core.usecase.classes.*;
 import com.liaw.dev.Recipe.infrastructure.dto.RecipeDTO;
 import com.liaw.dev.Recipe.infrastructure.mapper.RecipeMapper;
 import jakarta.validation.Valid;
@@ -20,10 +18,12 @@ import java.util.List;
 public class RecipeController {
 
     private final RecipeMapper mapper;
+    private final ListRecipeUseCaseImpl listRecipe;
     private final CreateRecipeUseCaseImpl createRecipe;
     private final UpdateRecipeUseCaseImpl updateRecipe;
     private final DeleteRecipeUseCaseImpl deleteRecipe;
-    private final ListRecipeUseCaseImpl listRecipe;
+    private final SearchRecipeUseCaseImpl searchRecipe;
+    private final SearchByCategoryUseCaseImpl searchByCategory;
 
     @GetMapping
     public ResponseEntity<List<RecipeDTO>> listRecipe(){
@@ -33,21 +33,41 @@ public class RecipeController {
         return ResponseEntity.ok(dtos);
     }
 
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<RecipeDTO>> searchByCategory(@PathVariable Category category){
+        List<Recipe> recipes = searchByCategory.execute(category);
+        List<RecipeDTO> dtos = recipes.stream().map(mapper::toDto).toList();
+        return ResponseEntity.ok(dtos);
+    }
+
+    @GetMapping("search/")
+    public ResponseEntity<List<RecipeDTO>> searchRecipe(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) String ingredient
+    ){
+        List<Recipe> recipes = searchRecipe.execute(title, description, ingredient);
+        List<RecipeDTO> dtos = recipes.stream()
+                .map(mapper::toDto).toList();
+
+        return ResponseEntity.ok(dtos);
+    }
+
     @PostMapping
     public ResponseEntity<RecipeDTO> createRecipe(@RequestBody @Valid RecipeDTO dto){
         Recipe recipe = mapper.toDomain(dto);
-        createRecipe.execute(recipe);
-        return ResponseEntity.ok(dto);
+        Recipe recipe_created = createRecipe.execute(recipe);
+        return ResponseEntity.ok(mapper.toDto(recipe_created));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<RecipeDTO> updateRecipe(@PathVariable Long id, @RequestBody @Valid RecipeDTO dto){
         Recipe recipe = updateRecipe.execute(id, mapper.toDomain(dto));
         RecipeDTO recipeDTO = mapper.toDto(recipe);
         return ResponseEntity.ok(recipeDTO);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<String> deleteRecipe(@PathVariable Long id){
         deleteRecipe.execute(id);
         return ResponseEntity.ok("Receita com id: "+id+" deletada com sucesso");
