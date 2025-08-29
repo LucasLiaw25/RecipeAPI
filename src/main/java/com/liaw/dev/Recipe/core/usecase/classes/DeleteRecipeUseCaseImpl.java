0f3e5b5a2e2f2cd1a90611186ab3b4b6e3700063
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 21:26:05 -0300

    feat: Implementa a interface de deletar receitas

--- src/main/java/com/liaw/dev/Recipe/core/usecase/classes/DeleteRecipeUseCaseImpl.java
+++ src/main/java/com/liaw/dev/Recipe/core/usecase/classes/DeleteRecipeUseCaseImpl.java
@@ -1,4 +1,19 @@
-package com.liaw.dev.Recipe.core.usecase;
+package com.liaw.dev.Recipe.core.usecase.classes;
 
-public class DeleteRecipeUseCaseImpl {
+import com.liaw.dev.Recipe.core.entities.Recipe;
+import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
+import com.liaw.dev.Recipe.core.usecase.interfaces.DeleteRecipeUseCase;
+
+public class DeleteRecipeUseCaseImpl implements DeleteRecipeUseCase {
+
+    private final RecipeGateway recipeGateway;
+
+    public DeleteRecipeUseCaseImpl(RecipeGateway recipeGateway) {
+        this.recipeGateway = recipeGateway;
+    }
+
+    @Override
+    public void execute(Long id) {
+        recipeGateway.deleteRecipe(id);
+    }
 }
