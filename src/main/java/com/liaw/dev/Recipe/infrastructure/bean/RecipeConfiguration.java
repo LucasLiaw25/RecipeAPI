ef54622efbc8b4b3b8eb80064ee3ccc9cdbf5209
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-29 12:01:02 -0300

    feat: Cria contexto de pesquisas de receita

--- src/main/java/com/liaw/dev/Recipe/infrastructure/bean/RecipeConfiguration.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/bean/RecipeConfiguration.java
@@ -1,10 +1,7 @@
 package com.liaw.dev.Recipe.infrastructure.bean;
 
 import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
-import com.liaw.dev.Recipe.core.usecase.classes.CreateRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.DeleteRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.ListRecipeUseCaseImpl;
-import com.liaw.dev.Recipe.core.usecase.classes.UpdateRecipeUseCaseImpl;
+import com.liaw.dev.Recipe.core.usecase.classes.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,6 +26,16 @@ public class RecipeConfiguration {
     @Bean
     public DeleteRecipeUseCaseImpl deleteRecipeUseCase(RecipeGateway gateway){
         return new DeleteRecipeUseCaseImpl(gateway);
+    }
+
+    @Bean
+    public SearchRecipeUseCaseImpl searchRecipeUseCase(RecipeGateway gateway){
+        return new SearchRecipeUseCaseImpl(gateway);
+    }
+
+    @Bean
+    public SearchByCategoryUseCaseImpl searchByCategoryUseCase(RecipeGateway gateway){
+        return new SearchByCategoryUseCaseImpl(gateway);
     }
 
 }
