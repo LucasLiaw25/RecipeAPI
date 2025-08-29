ec6fef70d7af898ea373b0e25e6564b9416e8e2b
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 21:28:36 -0300

    feat: Cria metodos de listar, atualizar e deletar receitas

--- src/main/java/com/liaw/dev/Recipe/infrastructure/gateway/RecipeRepositoryGateway.java
+++ src/main/java/com/liaw/dev/Recipe/infrastructure/gateway/RecipeRepositoryGateway.java
@@ -5,20 +5,52 @@ import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
 import com.liaw.dev.Recipe.infrastructure.mapper.RecipeEntityMapper;
 import com.liaw.dev.Recipe.infrastructure.percistence.RecipeEntity;
 import com.liaw.dev.Recipe.infrastructure.percistence.RecipeRepository;
+import com.liaw.dev.Recipe.infrastructure.validator.RecipeValidation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class RecipeRepositoryGateway implements RecipeGateway {
 
+    private final RecipeValidation validation;
     private final RecipeRepository repository;
     private final RecipeEntityMapper mapper;
 
     @Override
     public Recipe createRecipe(Recipe recipe) {
+        validation.existRecipeTitle(recipe.title(), recipe.description());
         RecipeEntity entity = mapper.toEntity(recipe);
         repository.save(entity);
         return recipe;
     }
+
+    @Override
+    public List<Recipe> listRecipe() {
+        List<RecipeEntity> recipeEntity = repository.findAll();
+        List<Recipe> recipes = recipeEntity.stream()
+                .map(mapper::toDomain).toList();
+        return recipes;
+    }
+
+    @Override
+    public Recipe updateRecipe(Long id, Recipe recipe) {
+        validation.existRecipeId(id);
+        RecipeEntity recipeEntity = mapper.toEntity(recipe);
+        recipeEntity.setId(id);
+        repository.save(recipeEntity);
+        return recipe;
+    }
+
+    @Override
+    public void deleteRecipe(Long id) {
+        validation.existRecipeId(id);
+        repository.deleteById(id);
+    }
+
+
 }
