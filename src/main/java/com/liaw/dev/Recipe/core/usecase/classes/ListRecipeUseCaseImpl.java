8400590f944794ca5617e6c222926d9e5b64ecc9
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-28 21:26:44 -0300

    feat: Implementa a interface de listar receitas

--- src/main/java/com/liaw/dev/Recipe/core/usecase/classes/ListRecipeUseCaseImpl.java
+++ src/main/java/com/liaw/dev/Recipe/core/usecase/classes/ListRecipeUseCaseImpl.java
@@ -1,12 +1,12 @@
-package com.liaw.dev.Recipe.core.usecase;
+package com.liaw.dev.Recipe.core.usecase.classes;
 
 import com.liaw.dev.Recipe.core.entities.Recipe;
 import com.liaw.dev.Recipe.core.gateway.RecipeGateway;
-import org.w3c.dom.events.Event;
+import com.liaw.dev.Recipe.core.usecase.interfaces.ListRecipeUseCase;
 
 import java.util.List;
 
-public class ListRecipeUseCaseImpl implements ListRecipeUseCase{
+public class ListRecipeUseCaseImpl implements ListRecipeUseCase {
 
     private final RecipeGateway recipeGateway;
 
